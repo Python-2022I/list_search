1f610e44780a8dd38587473ed4696da80b7a8045
ikromyusupov <@ikromyusupovdev@gmail.com>
2022-11-08 17:55:00 +0500

    done

---
+++ find06_max_odd.py
@@ -0,0 +1,8 @@
+def find_max_odd(data:list[int])->int:
+    """
+    Given the list of numbers, Find the maximum odd number in the list
+    args:
+        data: list of numbers
+    returns: maximum odd number in the list
+    """
+    return 0
